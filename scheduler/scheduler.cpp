22732f48cb546a92cae1a4861b7a944b02514456
wchang <yeckoh@gmail.com>
2020-02-09 11:14:19 -0800

    shooting in the dark

--- scheduler/scheduler.cpp
+++ scheduler/scheduler.cpp
@@ -9,6 +9,45 @@
 
 
 
+#include "../includes/scheduler.h"
+
+
+
+Scheduler::Scheduler(std::queue<PCB> &queue, bool preemptive = false, int time_slice =
+			UNINITIALIZED) :	ready_q(&queue), preemptive(preemptive), time_slice(time_slice) {}
+
+Scheduler::~Scheduler() {}
+
+//add a process, either a new one or one that
+//had been running on the CPU and has been preempted
+void Scheduler::add(PCB p) {
+	ready_q->push(p);
+}
+
+//get next process
+PCB Scheduler::getNext() {
+	return ready_q->pop();
+}
+
+//returns true if there are no  jobs in the readyQ
+//false otherwise
+bool Scheduler::isEmpty() {
+	return ready_q->empty();
+}
+
+//if process has completed (used all its remaining_cpu_time) or
+//if we are using a preemptive scheduling algorithm and the
+//timeslice is over then its time to switch processes
+//returns:
+//true - switch processes
+//false - do not switch
+bool Scheduler::time_to_switch_processes(int tick_count, PCB &p) {
+	// int process_number, arrival_time, required_cpu_time
+	return (!p.remaining_cpu_time || (preemptive && !time_slice));
+}
+
+// sort has no behavior in base class
+
 
 
 
--- scheduler/scheduler_FIFO.cpp
+++ scheduler/scheduler_FIFO.cpp
@@ -9,5 +9,17 @@
  */
 
 //TODO fill in content
+#include "../includes/scheduler_FIFO.h"
 
 
+Scheduler_FIFO::Scheduler_FIFO(std::queue<PCB> &queue):Scheduler(queue,false,UNINITIALIZED) {}
+
+Scheduler_FIFO::~Scheduler_FIFO() {}
+
+//override base class behaviour if necessary, otherwise call it
+bool Scheduler_FIFO::time_to_switch_processes(int tick_count, PCB &p) {
+	return this->Scheduler(&tick_count, &p);
+}
+
+//FIFO - not preemptive - no sorting needed
+void sort() {}
